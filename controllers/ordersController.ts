143940664c43c1d884f4bdfc3313353f36896f10
Brian Figueroa <bryan09figueroa@gmail.com>
2023-09-11 15:30:03 -0300

    correccion order id

--- controllers/ordersController.ts
+++ controllers/ordersController.ts
@@ -6,7 +6,7 @@ import Order, { IOrder } from "../model/ordersModel";
 export const getOrders = async (req: Request, res: Response): Promise<void> => {
     const userID: ObjectId = req.body.userConfirm._id;
 
-    const query = { user: userID }
+    const query = { userId: userID }
     const orders = await Order.find(query)
 
     res.json({
